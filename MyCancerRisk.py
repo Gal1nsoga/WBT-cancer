a6a3646f79f6cd8b327f2f8d3d017fe9e489518e
Gal1nsoga <130666212+Gal1nsoga@users.noreply.github.com>
2024-08-02 14:59:36 +0800

    Update MyCancerRisk.py

--- MyCancerRisk.py
+++ MyCancerRisk.py
@@ -8,7 +8,7 @@ import pickle
 
 # main function
 # 设置网页名称
-st.set_page_config(page_title='MyCancerRisk\n癌症风险预测工具')
+st.set_page_config(page_title='MyCancerRisk癌症风险预测工具')
 
 
 # 设置网页标题
@@ -145,7 +145,7 @@ def make_predict(input_df):
 
 
 # 设置一个按钮用于预测
-if st.button('Please click the button to predict.<br>点击进行预测'):
+if st.button('Please click the button to predict（请点击进行预测）'):
     # 检查是否完成了所有选项
     if input_df.isnull().values.any():
         st.warning("You have unfinished questions, please make sure you have completed all of them！\n您有问题未完成，请确保完成了所有选项！")
